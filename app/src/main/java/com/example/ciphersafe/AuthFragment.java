f2da7191409f51538d3a2f59385287a462eb64e5
Tahmid Chowdhury <tahmid1203@hotmail.com>
2025-03-02 11:43:24 -0500

    Add authentication fragment and layout

---
+++ app/src/main/java/com/example/ciphersafe/AuthFragment.java
@@ -0,0 +1,34 @@
+package com.example.ciphersafe;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AuthFragment extends Fragment {
+
+    private Button authButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_auth, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        authButton = view.findViewById(R.id.auth_button);
+        authButton.setOnClickListener(v -> {
+            if (getActivity() instanceof MainActivity) {
+                ((MainActivity) getActivity()).authenticateUser();
+            }
+        });
+    }
+}
